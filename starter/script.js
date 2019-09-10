19d2f882b173cf34181aa8809e83fc23e2216b48
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-09-10 22:23:02 +0100

    Primitive vs Objects

--- starter/script.js
+++ starter/script.js
@@ -34,6 +34,7 @@ console.log(cesar.lastName);
 
 // OBJECT.CREATE
 
+/*
 var personProto = {
     calculateAge: function(){
         console.log(2019 - this.yearOfBirth);
@@ -54,7 +55,51 @@ var jane = Object.create(personProto, {
     
 });
 
+*/
 
+// PRIMITIVE VS OBJECTS
+
+// PRIMITIVE HOLD THE DATA
+// IT IS A REFERENCE THAT POINT THE LOCATION IN MEMORY
+
+//PRIMITIVE
+var a = 29;
+var b = a;
+
+a = 46;
+
+console.log(a);
+console.log(b);
+
+//OBJECT
+var obj1 = {
+    name : 'Rodolfo',
+    age : 31
+};
+
+var obj2 = obj1;
+
+obj1.age = 45;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+// FUNCTION
+
+var age = 27;
+var obj = {
+    name : 'Jonas',
+    city : 'Lisbon'
+};
+
+function change(a, b){
+    a = 38;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
 
 
 
